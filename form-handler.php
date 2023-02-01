e93e467aa73700e0fb1f5a1d3aa1359d3ee289b5
derrick257 <83549044+derrick257@users.noreply.github.com>
2023-02-02 01:22:58 +0200

    Update form-handler.php

--- form-handler.php
+++ form-handler.php
@@ -4,7 +4,7 @@ $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
-$email_from = 'info@gofinduniversity.com';
+$email_from = 'info@derrick257.github.io';
 
 $email_subject = 'New Form Submission';
 
